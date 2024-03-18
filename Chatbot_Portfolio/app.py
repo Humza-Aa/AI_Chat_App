0b154b96113b73e86589866285c37eb57446cb06
Humza-Aa <humzaaamir31@gmail.com>
2024-03-18 17:50:04 -0400

    Added: CORS Updated: Intents.json

--- Chatbot_Portfolio/app.py
+++ Chatbot_Portfolio/app.py
@@ -1,16 +1,21 @@
 from flask import Flask, request, make_response
 from chat_init import chatbot_response
+from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/chat', methods=['POST'])
 def chat():
+    print(request)
     message = request.form['message']
     res = chatbot_response(message)
     response = make_response(res)
     response.headers['Content-Type'] = 'text/plain'
     return response
+
 
 if __name__ == '__main__':
     app.run(debug=True)
