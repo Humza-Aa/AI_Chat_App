adf7e439eadb61b5f30873592a81046cc27cc5e9
Humza-Aa <humzaaamir31@gmail.com>
2024-03-25 17:27:09 -0400

    Added: chat records

--- Chatbot_Portfolio/app.py
+++ Chatbot_Portfolio/app.py
@@ -2,12 +2,19 @@ from flask import Flask, request, make_response
 from chat_init import chatbot_response
 from flask import Flask
 from flask_cors import CORS
-import logging
+from pymongo import MongoClient 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MONGO_URI = os.environ.get("MONGO_URI")
+client = MongoClient(MONGO_URI)
+db = client.get_database()
+collection = db.logs
 
 app = Flask(__name__)
 CORS(app)
-
-logging.basicConfig(filename='app.log', level=logging.INFO)
 
 
 @app.route('/')
@@ -20,9 +27,13 @@ def chat():
     message = request.form['message']
     res = chatbot_response(message)
     response = make_response(res)
-    logging.info(f"Request: {request}")
-    logging.info(f"Message: {message}")
-    logging.info(f"Response: {res}")
+
+    request_response = {
+        'request': message,
+        'response': res
+    }
+    collection.insert_one(request_response)
+
     response.headers['Content-Type'] = 'text/plain'
     return response
 
