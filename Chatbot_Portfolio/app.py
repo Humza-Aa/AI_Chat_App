b1ba55e08c6cedd4f48c3e13ec9c45e800da6422
Humza-Aa <humzaaamir31@gmail.com>
2024-04-14 23:02:37 -0400

    added IP T

--- Chatbot_Portfolio/app.py
+++ Chatbot_Portfolio/app.py
@@ -25,6 +25,11 @@ CORS(app)
 def home():
     return "Hello!"
 
+@app.route('/test', methods=['Post'])
+def iptest():
+    print(request.__dict__)  # Print out the entire request object
+    client_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+    return f"Your IP address is: {client_ip}"
 
 def get_user_ip():
     if request.headers.getlist("X-Forwarded-For"):
